1fa333596716a1ed8e891ad82239bb4b525cc54a
osk0830 <oshiro.sk.0830@gmail.com>
2025-11-02 21:09:41 +0900

    ファイルの整形

--- it-builder/wp-content/themes/itbuilder-theme/resource/js/top.js
+++ it-builder/wp-content/themes/itbuilder-theme/resource/js/top.js
@@ -1,43 +1,44 @@
-
 //variables
 var tabWidth = 768;
 
 //リサイズ
-$(window).on('load resize', function(){
+$(window).on("load resize", function () {
   slideHuck();
   slideHuck2();
 });
 
-  
-function slideHuck(){
-  if ($(window).width() <= tabWidth) {//スマホ
-    $('.js-installationBox').slick({
-      dots:true,
-      arrows:true,
-      centerMode:true,
-      variableWidth: true
+function slideHuck() {
+  if ($(window).width() <= tabWidth) {
+    //スマホ
+    $(".js-installationBox").slick({
+      dots: true,
+      arrows: true,
+      centerMode: true,
+      variableWidth: true,
     });
   }
 }
 
-function slideHuck2(){
+function slideHuck2() {
   //slider
-  $('.js-keyvisual').slick({
-    autoplay:true,
-    dots:true,
-    centerMode:true,
+  $(".js-keyvisual").slick({
+    autoplay: true,
+    dots: true,
+    centerMode: true,
     variableWidth: true,
-    responsive: [{
-      breakpoint: 768,
+    responsive: [
+      {
+        breakpoint: 768,
         settings: {
-          arrows:true,
-        }
-    }]
+          arrows: true,
+        },
+      },
+    ],
   });
-  $('.js-serviceBox').slick({
-    dots:true,
-    arrows:true,
-    centerMode:true,
-    variableWidth: true
+  $(".js-serviceBox").slick({
+    dots: true,
+    arrows: true,
+    centerMode: true,
+    variableWidth: true,
   });
 }
